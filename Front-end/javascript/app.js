ee6f777dbb82004670467e2c9f2df722177d70fa
Soso-C <sonny.capisciolti@gmail.com>
2021-08-31 13:44:16 +0200

    2nd commit

--- Front-end/javascript/app.js
+++ Front-end/javascript/app.js
@@ -1,6 +1,6 @@
 let url = "http://localhost:3000/api/cameras"
 
 fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data));
-        document.getElementById('#name_article').innerHTML = data.name
+.then(res => res.json())
+.then(data => console.log(data));
+document.getElementById('#name_article').innerHTML = data.name;
