829e95f227d79249c628643ae4c94b87a6ec7826
Soso-C <sonny.capisciolti@gmail.com>
2021-09-01 14:48:16 +0200

    Update nouveautée

--- Front-end/javascript/app.js
+++ Front-end/javascript/app.js
@@ -3,4 +3,3 @@ let url = "http://localhost:3000/api/cameras"
 fetch(url)
 .then(res => res.json())
 .then(data => console.log(data));
-document.getElementById('#name_article').innerHTML = data.name;
