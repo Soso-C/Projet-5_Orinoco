2c046e6c4209201f2e03ba5ceb9004675f7408be
Soso-C <sonny.capisciolti@gmail.com>
2021-09-04 00:06:04 +0200

    update js

--- Front-end/javascript/app.js
+++ Front-end/javascript/app.js
@@ -1,24 +1,33 @@
-// let url = "http://localhost:3000/api/cameras"
+let url = "http://localhost:3000/api/cameras"
 
-// fetch(url)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     console.log(data);
-//     let affichage = "<ul>";
-//     for(let article of data){
-//         affichage += `<li>${article.name}</li>`
-//     }
-//     affichage += "</ul>"
-//     document.querySelector("#name_article").innerHTML = affichage; 
-//   })
-
-// fetch('http://localhost:3000/api/cameras')
-//   .then(response => response.json())
-//   .then(data => {
-//       const articlesContainer = document.getElementById('articles');
-//       for (let article of data) {
-//           articlesContainer.innerHTML += `${article.name} ${article.price}<br>`;
-//       }
-//   });
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let name ;
+    let price;
+    let id;
+    let desc;
+    let image;
+    let lense;
+    let i = 0;
+    for(let article of data){
+        name = `${article.name}`
+        price = `${article.price}`
+        id = `${article._id}`
+        desc = `${article.description}`
+        image = `${article.imageUrl}`
+        lense = `${article.lenses}`
+        console.log(name)
+        console.log(price)       
+        console.log(id)       
+        console.log(desc)       
+        console.log(image)       
+        console.log(lense)       
+        document.getElementsByClassName("name_article")[i].innerHTML = name;
+        document.getElementsByClassName("price_article")[i].innerHTML = price / 100 +"€";
+        i++
+    }    
+  })
