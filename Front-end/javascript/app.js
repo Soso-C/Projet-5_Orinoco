fab340f38e8548bb0bace1587e731667010a3f90
Soso-C <sonny.capisciolti@gmail.com>
2021-09-03 15:44:08 +0200

    update et ajout Accueil main page

--- Front-end/javascript/app.js
+++ Front-end/javascript/app.js
@@ -14,3 +14,11 @@
 //     document.querySelector("#name_article").innerHTML = affichage; 
 //   })
 
+fetch('http://localhost:3000/api/cameras')
+  .then(response => response.json())
+  .then(data => {
+      const articlesContainer = document.getElementById('articles');
+      for (let article of data) {
+          articlesContainer.innerHTML += `${article.name} ${article.price}<br>`;
+      }
+  });
