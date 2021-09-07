835881b7860d41eeec334536495a1653ae59f81d
Soso-C <sonny.capisciolti@gmail.com>
2021-09-07 15:16:44 +0200

    update refonte et ajout page product

--- Front-end/javascript/article_id.js
+++ Front-end/javascript/article_id.js
@@ -1,3 +1,4 @@
+
 (async function(){
     const articleId = getArticleId()
     const article = await getArticle(articleId)
@@ -22,37 +23,37 @@ function getArticle(articleId){
 function hyrdrateArticle(article) {
     document.getElementById("test1").innerHTML +=
     `
-    <div><img class="img_article" src="${article.imageUrl}" alt=""></div>
+    <div><img class="img_product" src="${article.imageUrl}" alt=""></div>
     <div class="art_name_price">
         <p class="name_article">${article.name}</p>
-        <p class="desc_article">${article.description}</p>
-    </div>
-    <div class="bloc_p_t_quant">
-        <div class="quant_nmb">
-            <p>Quantitée</p>
-            <select name="p_quant">
-                <option> 1
-                <option> 2
-                <option> 3
-                <option> 4
-                <option> 5
-                <option> 6
-                <option> 7
-                <option> 8
-                <option> 9
-            <select>
-        </div>    
-    </div>   
+        <p class="desc_product">${article.description}</p>
+    </div>  
     <div class="quant_nmb">
-    <p>Lentilles</p>
+    <p class="lent" >Lentilles :</p>
     <select name="p_quant">
         <option> ${article.lenses}
         <option> ${article.lenses[1]}
     <select>
     </div>
+    <div class="bloc_p_t_quant">
+        <div class="quant_nmb">
+            <p class="quant">Quantitée :</p>
+            <form class="btn_-_+">
+                <input id="decrease" type="button" value="-"/>
+                <input id="result" type="texte" value="1" maxlength="2"/>
+                <input id="increase" type="button" value="+"/>
+            </form>
+        </div>
+        <p class="price_product">Prix : ${article.price / 100}€</p>    
+    </div> 
     <div class="btn_price">
-    <a href="#"><button type="submit" class="btn_test">Ajouter au panier</button></a>
-    <p class="price_article">Prix : ${article.price / 100}€</p>
+        <a href="#"><button type="submit" class="btn_test2">Ajouter au panier</button></a>
     </div>`
     
 }
+
+// Button + - //
+
+let res = document.getElementById('result');
+let plus = document.getElementById('increase');
+let moins = document.getElementById('decrease');
