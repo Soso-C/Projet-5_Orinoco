dd72c848a06d6db63530d34e6823c3ce8f116333
Soso-C <sonny.capisciolti@gmail.com>
2021-10-07 16:43:53 +0200

    responsive tablette / mobile

--- Front-end/javascript/cart.js
+++ Front-end/javascript/cart.js
@@ -35,53 +35,53 @@ function decreaseValue() {
 
 function makeCart(){
     let test = JSON.parse(localStorage.getItem("product"));
+    let prixTotal = 0;
 
     document.querySelector(".bloc_cart").innerHTML += `<h2>Mon panier</h2>`;
 
     for (produit in test) {
+
+        document.querySelector(".bloc_cart").innerHTML +=`
         
-        document.querySelector(".bloc_cart").innerHTML +=
-        `
         <div class="cart_elt_cont">
             <div class="cart_img_title">
                 <img class="img_cart_art" src=${test[produit].imgUrl} alt="Photo camera">
             </div>
-            <div>
+            <div class="cart_text_cont">
                 <p>Nom</p>
                 <h6>${test[produit].name}</h6>
             </div>
-            <div>
+            <div class="cart_text_cont">
                 <p>Quantitée</p>
                 <h6>${test[produit].quantity}</h6>
             </div>
-            <div>
+            <div class="cart_text_cont">
                 <p>Lentille</p>
                 <h6>${test[produit].lense}</h6>
             </div>
-            <div>
+            <div class="cart_text_cont">
                 <p>Prix</p>
                 <h6>${test[produit].price}</h6>
             </div>
-            <div>
+            <div class="cart_text_cont">
                 <p>Total</p>
                 <h6 class="total_price_elt">${parseInt(test[produit].price) * parseInt(test[produit].quantity)} €</h6>
             </div>
             <div class="cart_item_clear" onclick="clearCart()"><i class="fas fa-trash-alt"></i></div>
-                
             </div>
         </div>`
+
+        prixTotal += (parseInt(test[produit].price) * parseInt(test[produit].quantity));
     };
     document.querySelector(".bloc_cart").innerHTML += `
     <div class="cart_total">
       <p>Prix Total :</p>
-      <p class="p_total">${totalPrice} €</p>
+      <p class="p_total">${prixTotal}€</p>
     </div>`;
-    
 }
 
 // Clear notre cart le localstorage et refresh la page.
 function clearCart() {
-  document.querySelector('.bloc_cart').innerHTML = "";
   localStorage.clear();
   window.location.reload();
 }
@@ -111,6 +111,10 @@ function makeForms() {
            <input type="text" id="fname" name="fname" placeholder="Entrez votre prénom" required/>
        </div>
        <div class="form_group">
+           <label for="zip_code">Code Postale</label>
+           <input type="text" id="zip_code" name="user_zip" placeholder="Ex: 69000" pattern="[0-9]{5}" required/>
+       </div>
+       <div class="form_group">
            <label for="adress">Adresse</label>
            <input type="text" id="adress" name="user_adress" placeholder="Entrez votre adresse" required/>
        </div>
@@ -120,7 +124,7 @@ function makeForms() {
        </div>
        <div class="form_group">
            <label for="tel">Telephone</label>
-           <input type="tel" id="tel" name="user_tel" placeholder="06 06 06 06 07" pattern="[0-9]{10}" required/>
+           <input type="tel" id="tel" name="user_tel" placeholder="Ex: 06 06 06 06 06" pattern="[0-9]{10}" required/>
        </div>
        <div class="btn_valider">
            <a href="#"><button type="submit" id="btn_test2">Commander</button></a>
